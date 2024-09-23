8fa2b656cd1618a0d68dcc5e711eb749d2fba7c6
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2024-09-23 18:08:00 -0300

    feat: Adicionado as structs na Main
    Adicionado as funções cadastro e ExibirMenuCliente, além da struct que estava na main.

--- funcoes.h
+++ funcoes.h
@@ -1,7 +1,42 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
+#include <time.h>
+
+typedef struct
+{
+    char cpf[11];
+    char senha[255];
+    float saldoReais;
+    double saldoBTC;
+    double saldoETH;
+    double saldoRIPPLE;
+
+}Usuario;
+
+typedef struct{
+
+    float cotacao;
+    float txVenda;
+    float txCompra;
+
+}Criptomoedas;
+
+typedef struct{
+
+    int qttExtratos;
+    float saldoReais;
+    double saldoBTC;
+    double saldoETH;
+    double saldoRIPPLE;
+    time_t Data;
+
+}Extratos;
+
+
 
 int ExibirMenuVisitante();
 void limpaBuffer();
+int ExibirMenuCliente();
+int cadastro(Usuario *ptrUsuario);
 
 #endif
