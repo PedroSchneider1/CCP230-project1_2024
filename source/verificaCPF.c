dce5d237b7b6701a311a6b7c8e00fbf7988d0193
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-09-25 15:40:13 -0300

    fix: implementações para a função depositar

--- source/verificaCPF.c
+++ source/verificaCPF.c
@@ -6,6 +6,7 @@ int verificaCPF(Usuario *ptrUsuario){
 
     FILE *ptrArquivo;
     int bytes = sizeof(Usuario);
+    long posicaoArquivo;
 
     // Abrir o arquivo para leitura
     ptrArquivo = fopen("clientes.bin", "rb");
@@ -16,21 +17,34 @@ int verificaCPF(Usuario *ptrUsuario){
         if (ptrArquivo == NULL)
         {
             perror("Erro ao criar o arquivo");
-            return 2;
+            return -1;
         }
+        //arquivo vazio, retorna -1
+        fclose(ptrArquivo);
+        return -1;
     }
 
     Usuario usuario;
 
     while(fread(&usuario, bytes, 1, ptrArquivo) == 1)
     {
+        posicaoArquivo = ftell(ptrArquivo) - bytes;
         if(strcmp(usuario.cpf, ptrUsuario->cpf) == 0)
         {
+            //passa os valores do arquivo para a struct
+            strcpy(ptrUsuario->cpf, usuario.cpf);
+            strcpy(ptrUsuario->senha, usuario.senha);
+            ptrUsuario->saldoReais = usuario.saldoReais;
+            ptrUsuario->saldoBTC = usuario.saldoBTC;
+            ptrUsuario->saldoETH = usuario.saldoETH;
+            ptrUsuario->saldoRIPPLE = usuario.saldoRIPPLE;
+            
+            
             fclose(ptrArquivo);
-            return 1;
+            return posicaoArquivo;
         }
     }
 
     fclose(ptrArquivo);
-    return 0;
+    return -1;
 }
